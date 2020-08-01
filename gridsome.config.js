7aad06f980443c29de6833fc7d78fcf24da78955
mimoid-prog <mimoid.prog@gmail.com>
2020-08-01 09:33:57 +0200

    Added google sitemap

--- gridsome.config.js
+++ gridsome.config.js
@@ -8,6 +8,33 @@ module.exports = {
         id: "UA-156522420-5",
       },
     },
+    {
+      use: "@gridsome/plugin-sitemap",
+      options: {
+        config: {
+          "/aktualnosci": {
+            changefreq: "weekly",
+            priority: 0.9,
+            lastmod: "2020-08-01",
+          },
+          "/o-klubie": {
+            changefreq: "yearly",
+            priority: 0.6,
+            lastmod: "2020-07-25",
+          },
+          "/terminarz": {
+            changefreq: "weekly",
+            priority: 0.8,
+            lastmod: "2020-08-01",
+          },
+          "/informacje": {
+            changefreq: "yearly",
+            priority: 0.7,
+            lastmod: "2020-07-25",
+          },
+        },
+      },
+    },
   ],
   chainWebpack: (config) => {
     const svgRule = config.module.rule("svg");

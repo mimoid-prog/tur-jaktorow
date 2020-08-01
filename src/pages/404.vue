142183697b79282aa617e3a46b3ee6fe34ed0d01
mimoid-prog <mimoid.prog@gmail.com>
2020-08-01 10:04:41 +0200

    Added 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,29 @@
+<template>
+  <Layout>
+    <h2 class="secondaryTitle viewTitle">Błąd 404</h2>
+    <h2 class="viewTitle">nie ma takiej podstrony</h2>
+    <Warning class="warning-icon" />
+  </Layout>
+</template>
+
+<script>
+import Warning from "~/assets/svgs/warning.svg";
+
+export default {
+  metaInfo: {
+    title: "Błąd 404",
+  },
+  name: "404",
+  components: {
+    Warning,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.warning-icon {
+  width: 100px;
+  display: block;
+  margin: 10px auto 0;
+}
+</style>
